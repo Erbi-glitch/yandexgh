47cc355d4cda873eacd132b3e64a0c59e7cbd0e5
GD-TG <83457537+GD-TG@users.noreply.github.com>
2023-04-28 00:09:37 +0500

    Alpha 8.0 Add map mouse find

--- backend.py
+++ backend.py
@@ -14,9 +14,10 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.map_scale = 0.001
         self.map_x = 0
         self.map_y = 0
+        self.point_position_x = None
+        self.point_position_y = None
         self.map_points = []
         self.is_find = False
-
 
         self.setupUi(self)
         self.pushButton.clicked.connect(self.find_point)
@@ -31,6 +32,7 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.horizontalSlider.valueChanged.connect(self.map_change_scale)
         self.nam = Qt.QNetworkAccessManager()
         self.nam.finished.connect(self.finish_request)
+        self.point_position_global = None
 
     def getImage(self):
 
@@ -41,7 +43,9 @@ class MyWidget(QMainWindow, Ui_MainWindow):
             '3': 'l=sat,skl',  # гибрид
             '4': 'l=sat,trf,skl'  # гибридная карта с отображением пробок
         }
+
         location = self.lineEdit.text()
+
         georesponse = requests.get(
             f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={location}&format=json")
         json_response = georesponse.json()
@@ -49,17 +53,18 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         # Получаем первый топоним из ответа геокодера.
         # Согласно описанию ответа, он находится по следующему пути:
         toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        self.point_position_global = toponym['boundedBy']['Envelope']
         # Название:
         toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
         # Координаты:
         toponym_coordinates = toponym["Point"]["pos"].split()
         print(toponym_address)
         print(toponym_coordinates)
+
         y = str(float(toponym_coordinates[0]) + self.map_y)
         x = str(float(toponym_coordinates[1]) + self.map_x)
-        print(toponym_coordinates)
-        print(self.mail_true.isChecked())
-
+        self.point_position_y = float(y)
+        self.point_position_x = float(x)
         if self.mail_true.isChecked():
             mail = toponym['metaDataProperty']['GeocoderMetaData']['Address']
             if 'postal_code' in mail.keys():
@@ -75,8 +80,9 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         if self.is_find:
             self.map_points.append(','.join(toponym_coordinates))
             self.is_find = False
-        print(coordinate)
-        print(type_map[self.map_change_type()])
+        # print(coordinate)
+        # print(toponym['boundedBy']['Envelope'])
+        # print(type_map[self.map_change_type()])
         if self.map_points:
             self.map_request = f"http://static-maps.yandex.ru/1.x/?ll={coordinate}&spn={self.map_scale},{self.map_scale}&{type_map[self.map_change_type()]}&pt={'~'.join(self.map_points)},comma"
         else:
@@ -92,7 +98,9 @@ class MyWidget(QMainWindow, Ui_MainWindow):
             return '0'
 
     def map_change_scale(self, value):
+
         self.map_scale = value / 1000
+        print(self.map_scale)
         self.on_load()
 
     def map_change_coordinates(self):
@@ -122,6 +130,7 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         print("Load image")
         self.getImage()
         url = self.map_request
+
         self.nam.get(Qt.QNetworkRequest(Qt.QUrl(url)))
 
     def finish_request(self, reply):
@@ -136,9 +145,48 @@ class MyWidget(QMainWindow, Ui_MainWindow):
             self.textBrowser.setText('Вы не ввели адрес')
         print('Update')
 
+    def mousePressEvent(self, event):
+        if self.point_position_global and 273 < event.x() < 749 and 102 < event.y() < 487:
+            try:
+
+                qx = round(self.point_position_x + self.map_scale - (event.x() / 749) * self.map_scale, 7)
+                qy = round(self.point_position_y + self.map_scale - (490 / event.y()) * self.map_scale + 0.0004, 7)
+                # print(self.point_position_x,self.map_scale,(event.x()/749)*self.map_scale*2)
+                print(qx, qy)
+                geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={str(qy)},{str(qx)}&format=json"
+                response = requests.get(geocoder_request)
+                json_response = response.json()
+                toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+                toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+
+                print(toponym_address)
+                self.textBrowser.append(f'\n Вы выбрали {toponym_address}')
+                point_position_x = float(self.point_position_global['lowerCorner'].split()[0]) * (
+                        1.01 + self.map_scale / 100)
+                point_position_y = float(self.point_position_global['lowerCorner'].split()[1]) * (
+                        1.01 + self.map_scale / 100)
+                # print(point_position_x,point_position_y, event.x(), event.y())+0.00001
+                '''
+                
+                print(point_position_x,point_position_y)
+                print(point_position_x * (1.01 + self.map_scale / 100),
+                      point_position_y * (1.02 + self.map_scale / 100))
+
+                x_kray=point_position_x * (1.01 + self.map_scale / 100)
+                y_kray=point_position_y * (1.02 + self.map_scale / 100)
+
+                print(f"Координаты:{ (event.x() / 487 )* point_position_x * (1.01 + self.map_scale / 100)}, { (749/event.y() ) * point_position_y * (1.02 + self.map_scale / 100)}", )'''
+            except Exception as e:
+                print(e)
+
 
 if __name__ == '__main__':
     ap = QApplication(sys.argv)
     scr1 = MyWidget()
     scr1.show()
     sys.exit(ap.exec())
+'''
+[[54.63874909348456,55.62914664782716],[54.8312557744172,56.288326335327156]]►
+[[53.95771679524626,53.32201774157716],[55.497817707334384,58.59545524157716]]
+[[54.734365297279616,55.95616157092289],[54.73586925498853,55.96131141223149]]
+'''
